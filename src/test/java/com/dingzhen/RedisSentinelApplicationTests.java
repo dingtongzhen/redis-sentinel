c9e5fa803c4fcee5e2e58ebc569904359f07c373
丁真 <1945593906@qq.com>
2020-08-08 11:32:37 +0800

    redis-sentinel

--- src/test/java/com/dingzhen/RedisSentinelApplicationTests.java
+++ src/test/java/com/dingzhen/RedisSentinelApplicationTests.java
@@ -1,7 +1,12 @@
 package com.dingzhen;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootTest
 class RedisSentinelApplicationTests {
@@ -9,5 +14,22 @@ class RedisSentinelApplicationTests {
     @Test
     void contextLoads() {
     }
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    ValueOperations<String, String> stringRedis;
+
+    @PostConstruct
+    public void init(){
+        stringRedis=redisTemplate.opsForValue();
+    }
+
+
+    @Test
+    public void testString (){
+        stringRedis.set("name", "dingzhen");
+        System.out.println(stringRedis.get("name"));
+    }
+
 
 }
